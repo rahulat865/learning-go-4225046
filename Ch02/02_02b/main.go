f25a66f75aec058824ffdcb2e7891dea7a7217f9
Rahul Raj <168128675+rahulat865@users.noreply.github.com>
2026-09-17 18:20:03 +0000

    update the chapter 2

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,11 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 92;
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1 , str2 , str3)
+	fmt.Println("Number is:" , aNumber)
 }
--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	str, _ := reader.ReadString('\n')
+	fmt.Println(str)
+
+
+	fmt.Print("Enter a number")
+	str, _ = reader.ReadString('\n')
+	cd
 
 }
